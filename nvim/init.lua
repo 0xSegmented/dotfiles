84f09e0316d96eeef3c660c1b2efd7d532e5c20a
xatt <111096156+MaliciousXatt@users.noreply.github.com>
2023-06-14 13:44:57 +0200

    Add files via upload
    Nvim dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("core.options")
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+
+
+vim.cmd("highlight Normal guibg=none ctermbg=none")
+vim.cmd("highlight NonText guibg=none ctermbg=none")
+vim.cmd("highlight SignColumn guibg=none ctermbg=none")
+vim.cmd("highlight EndOfBuffer guibg=none ctermbg=none")
